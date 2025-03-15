43dcce10fe80740fa8050bc389635495444de957
Nick Greenfield <nagreenfield@yahoo.com>
2025-03-15 11:26:40 -0700

    Class 3 and css

--- posts/urls.py
+++ posts/urls.py
@@ -5,4 +5,5 @@ from posts import views
 urlpatterns = [
     path("", views.PostListView.as_view(), name="list"),
     path("<int:pk>/", views.PostDetailView.as_view(), name="detail"),
+    path("new/", views.PostCreateView.as_view(), name="new"),
 ]
